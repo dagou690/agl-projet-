b616f2a09492dbd4ef7e7eaf816f30dd1438d4b4
Emmanuel Coulibaly <coulibaly45emmanuel@gmail.com>
2025-01-13 23:05:26 +0000

    Ajout des modèles locaux avant pull

---
+++ app/Models/Film.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+    use HasFactory;
+
+    // Définir les colonnes qui peuvent être remplies automatiquement
+    protected $fillable = ['code','titre', 'date_sortie', 'sujet', 'realisateur_id', 'producteur_id','cover'];
+
+    // Relation avec le modèle Réalisateur
+    public function realisateur()
+    {
+        return $this->belongsTo(Realisateur::class);
+    }
+
+    // Relation avec le modèle Producteur
+    public function producteur()
+    {
+        return $this->belongsTo(Producteur::class);
+    }
+}
